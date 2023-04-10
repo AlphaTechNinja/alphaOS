3212edf328a935943c84968e18b1993a96b76ece
AlphaTechNinja <114359889+AlphaTechNinja@users.noreply.github.com>
2023-04-10 10:44:51 -0700

    Update boot.lua

--- alphaOS/lib/core/boot.lua
+++ alphaOS/lib/core/boot.lua
@@ -1,6 +1,6 @@
 local raw_loadfile = ...
 local error = raw_loadfile("lib/error.lua").error
-local shell = raw_loadfile("lib/core/shell.lua").new()
+local shell = raw_loadfile("lib/core/shell.lua")
 function raw_dofile(file)
 local result,err = pcall(raw_loadfile,file)
 if not result and err then

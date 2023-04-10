d3543b661096dd9893edf5e36df10867cb8d40c6
AlphaTechNinja <114359889+AlphaTechNinja@users.noreply.github.com>
2023-04-10 06:42:46 -0700

    Update init.lua

--- alphaOS/init.lua
+++ alphaOS/init.lua
@@ -1,3 +1,14 @@
 do
-print("an error has occurred please reboot")
+_G.bootfile = "lib/core/boot.lua"
+_G.rootfilesystem = component.list("filesystem")()
+function load_file(file)
+local file = component.provoke(_G.rootfilesystem,"open",file,"r")
+local buffer = ""
+local line = ""
+until line == nil
+line = component.provoke(_G.rootfilesystem,"read",file,math.huge)
+buffer = buffer.." "..line
+repeat
+load(buffer,=init)
+init()
 end
